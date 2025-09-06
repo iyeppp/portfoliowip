cdb4c106db41defe193bee19e75e68518e81aa24
iyep <miawspecialist@gmail.com>
2025-09-06 10:55:39 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-// Pilih tombol hamburger dan navigasi
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.navbar');
 
-// Tambahkan event listener untuk 'click'
+
 menuToggle.addEventListener('click', function() {
-    // Toggle class 'active' pada navigasi
     nav.classList.toggle('active');
 });
